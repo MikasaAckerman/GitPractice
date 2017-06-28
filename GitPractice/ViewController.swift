97c1d215d127ae46139a0cd6ea745e3ecedaedbe
developer2 <developer2@easynew.jp>
2017-06-28 18:13:27 +0900

    create

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by 王淵博 on 2017/06/28.
+//  Copyright © 2017年 王淵博. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let a = 1
+        let b = 2
+        let c = 3
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
