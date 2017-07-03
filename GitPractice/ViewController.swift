9ca9a671c8865b6acf29666058277a5841dced47
developer2 <developer2@easynew.jp>
2017-07-03 10:34:17 +0900

    7.3.5

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let AAAA = -3
+        
         let AAA = -2
         
         let AA = -1
